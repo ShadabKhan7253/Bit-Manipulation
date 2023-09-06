8aba2b5d54e9fadfe91a73f9bc79a421558a675f
Shadab Khan <shadabkhan7253@gmail.com>
2023-09-06 12:47:32 +0530

    Setting the bit

---
+++ setBit.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+// Step:
+// 1. Bit masking: 1<<i
+// 2. Operation : OR
+
+int setBit(int n,int pos) {
+    return ((n & (1<<pos)) != 0);
+}
+
+int main() {
+    cout<<setBit(5,2);
+    return 0;
+}
