39819a00e3dd0cc4b44b44f8d99e537a04df65db
Shadab Khan <shadabkhan7253@gmail.com>
2023-09-06 13:24:39 +0530

    Updating bit

---
+++ updateBit.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+// Update Bit depend on 2 condition:
+// 1. To update 0 to 1
+//     In this case we use setBit function
+// 2. To update 1 to 0
+//     In this case we use clearBit function 
+// Both the method/function we have studied previosly
+
+int updateBit(int n,int pos,int operation) {
+    int num = 0;
+    if(operation == 1) {
+        num = n | (1<<pos);
+    } else {
+        int bitmask = 1<<pos;
+        int notBitMask = ~(bitmask);
+        num = n & notBitMask;
+    }
+    return num;
+}
+
+int main() {
+    cout<<updateBit(5,2,1)<<endl;
+    cout<<updateBit(5,2,0);
+    return 0;
+}
